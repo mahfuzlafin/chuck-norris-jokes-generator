30423761638d249274f0f6af6aebf4c34b33058c
Mahfuz Lafin <64304899+mahfuzlafin@users.noreply.github.com>
2020-06-03 15:50:39 +0600

    refactoring codebase

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-
-document.querySelector('form').addEventListener('submit', function(event){
+document.querySelector('form').addEventListener('submit', function (event) {
     const value = document.querySelector('input').value;
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `https://api.icndb.com/jokes/random/${value}`, true);
-    xhr.onload = function(){
-        if(this.status === 200){
+    xhr.onload = function () {
+        if (this.status === 200) {
             const response = JSON.parse(this.responseText);
             ///////////////////////////////////////////////////////////////////////////////////////////
             let output = '';
-            if(response.type === 'success'){
-                response.value.forEach(function(current){
+            if (response.type === 'success') {
+                response.value.forEach(function (current) {
                     output += `<li>${current.joke}</li>`;
                 });
-            }else{
+            } else {
                 output += '<li>Something went wrong</li>';
             }
             document.querySelector('ul').innerHTML = output;
             document.querySelector('input').value = '';
             ///////////////////////////////////////////////////////////////////////////////////////////
         }
-    }
+    };
     xhr.send();
     event.preventDefault();
 });
